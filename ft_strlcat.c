fa5ff550b9364d1223bf00428056e4548e0fb889
karimch-50 <karimchaouki50@gmail.com>
2022-09-15 22:13:01 +0100

    ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,48 @@
+#include <unistd.h>
+#include <bsd/string.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int count;
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	return count;
+} 
+
+int ft_strlcat(char *dest, char *src, int size)
+{
+	int dest_len;
+	int src_len;
+	int ret;
+	int i;
+
+	i = 0;
+	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dest);
+
+	if (dest_len > size)
+		ret = src_len + size;
+	else
+		ret = dest_len + src_len;
+	
+	while (src[i] != '\0' && dest_len < size - 1)
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
+	dest[dest_len] = '\0';
+	return ret;
+}
+
+
+int main()
+{
+	char dest[] = "hello\0";
+	char src[] = " my name is karim\0";
+	printf("%d\n", ft_strlcat(dest, src, 8));
+	puts(dest);
+	return (0);
+}
