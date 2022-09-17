612c1204920ec4f9e5bba7f8fe58575b3a698e77
karimch-50 <karimchaouki50@gmail.com>
2022-09-17 16:54:46 +0100

    ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+char *ft_memcpy(char *dest, const char *src, int n)
+{
+	int i;
+	i = -1;
+	if(n)
+	{
+		while (i++ < n)
+			dest[i] = src[i];
+		dest[i] = '\0';
+	}
+	return dest;
+}
