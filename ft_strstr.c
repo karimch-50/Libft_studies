329557044b5363d410a25671210890a9c8952878
karimch-50 <karimchaouki50@gmail.com>
2022-09-17 13:04:43 +0100

    ft_strstr.c

---
+++ ft_strstr.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+char *ft_strstr(char *str, char *tofind)
+{
+	int i;
+	int j;
+	i = 0;
+	if (!tofind)
+		return((char *)&str[i]);
+
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (tofind[j] == str[i + j])
+		{
+			j++;
+			if (tofind[j] == '\0')
+				return(&str[i]);
+		}
+		i++;
+	}
+	return (0);
+}
