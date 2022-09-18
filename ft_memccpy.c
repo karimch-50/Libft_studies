ae64b07021a69656d7d625fa49b1071fadd52438
karimch-50 <karimchaouki50@gmail.com>
2022-09-18 15:09:13 +0100

    ft_memccpy.c

---
+++ ft_memccpy.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+char *ft_memccpy(char *dest, char *src, int c, int n)
+{
+	int i;
+
+	i = -1;
+	while (i < n && src[i] != c)
+		dest[i++] = src[i];
+	return (dest);
+}
+
+// testing 
+
+// int main()
+// {
+// 	char dest[20];
+// 	char src[] = "Hello World";
+// 	memccpy(dest, src, 'x' , 11);
+// 	puts(dest);
+// 	return (0);
+// }
