549580c8027e7217eaf2c65111ab51548f4d920f
karimch-50 <karimchaouki50@gmail.com>
2022-09-19 23:20:21 +0100

    ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+//void *memmove(void *dest, const void *src, size_t n);
+char *ft_memmove(char *dest, const char *src, int n)
+{
+	int i;
+	i = 0;
+	// if (dest > src)
+	// 	while (n-- > 0)
+	// 		dest[n] = src[n];
+	// else
+		while (i < n)
+			dest[i++] = src[i];
+	return dest;
+}
+
+// testing
+
+// int main()
+// {
+// 	char dest[20];
+// 	char str[] = "hello world\0";
+// 	ft_memmove(dest, str,8);
+// 	//puts(dest);
+// 	puts(str);
+// 	return (0);
+// }
+
+
