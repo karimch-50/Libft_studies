0ec3c4e17ad3e6b99147c2d2e4eca9611388803c
karimch-50 <karimchaouki50@gmail.com>
2022-09-20 00:47:32 +0100

    ft_atoi.c

---
+++ ft_atoi.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_atoi(char *str)
+{
+	int sign;
+	int i;
+	int ret;
+	
+	ret = 0;
+	sign = 1;
+	i = 0;
+
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v' || str[i] == '\r' || str[i] == '\f')
+		i++;
+	if(str[i] == '-' || str[i] == '+')
+	{
+		if(str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
+		ret = ret * 10 + str[i++] - '0';
+	return ret * sign;
+}
+
+// testing
+
+// int main()
+// {
+// 	char str[] = "  a123a45";
+// 	printf("%d\n%d\n", atoi(str), ft_atoi(str));
+// 	return (0);
+// }
