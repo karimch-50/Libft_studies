0a2e41cd9a1324add805d3fb0ba3584c5e63032f
karimch-50 <karimchaouki50@gmail.com>
2022-09-21 00:43:04 +0100

    ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <bsd/string.h>
+
+//char *ft_strnstr(const char *big, const char *little, size_t len);
+
+
+char *ft_strnstr(char *str, char *tofind, int n)
+{
+	int i;
+	int j;
+
+	i =0;
+	if (!tofind[i])
+		return &str[i];
+	
+	while (str[i] != '\0' && i < n)
+	{
+		j = 0;
+		while (str[j + i] == tofind[j])
+		{
+			j++;
+			if(tofind[j] == '\0')
+				return &str[i];
+		}
+		i++;
+	}
+	return (0);
+}
+
+int main()
+{
+	char str[] = "Hello my name is karim";
+	char tofind[] = "";
+	puts(ft_strnstr(str, tofind, 5));
+
+	return (0);
+}
+
+// gcc ft_strnstr.c -lbsd
