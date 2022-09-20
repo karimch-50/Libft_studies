15e19e3fb8c8e7324ccddbc4d8d269746a609ab1
karimch-50 <karimchaouki50@gmail.com>
2022-09-20 21:01:00 +0100

    ft_strchr.c

---
+++ ft_strchr.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+char *ft_strchr(const char *s, int c)
+{
+	int i;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (char *)&s[i];
+		i++;
+	}
+	return 0;	
+}
+
+// testing
+
+// int main()
+// {
+// 	char str[12] = "Hello world";
+// 	puts(ft_strchr(str,'o'));
+// 	puts(strchr(str,'o'));
+// 	return (0);
+// }
